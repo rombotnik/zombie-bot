4dbc97cbf8a2cea1f529836d0292f8c1bdfdf371
Rom Stevens <romney.stevens@gmail.com>
2016-09-30 23:40:18 -0500

    Remove unnecessary new game command

--- zombie-bot/commands/game.rb
+++ zombie-bot/commands/game.rb
@@ -1,10 +1,6 @@
 module ZombieBot
   module Commands
     class Game < SlackRubyBot::Commands::Base
-      command 'new game' do |client, data, _match|
-        client.say(channel: data.channel, text: "Starting a new game with #{data['user']} as leader. Say *join* to join!")
-      end
-
       command 'test' do |client, data, _match|
         client.say(channel: data.channel, text: 'Braiiins ...')
       end
